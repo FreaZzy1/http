b0348db620ad7759b57ad870d0aa291558493350
artem <artem@FATT>
2025-03-25 23:40:32 +0500

    создание метода GetContent.Черемных

--- HttpNewsPAT_Cheremnykh/Program.cs
+++ HttpNewsPAT_Cheremnykh/Program.cs
@@ -63,5 +63,24 @@ namespace HttpNewsPAT_Cheremnykh
             
             Console.WriteLine(responseFromServer);
         }
+        public static void GetContent(Cookie Token)
+        {
+            // Задаём URL
+            string url = "http://news.permaviat.ru/main";
+            Debug.WriteLine($"Выполняем запрос: {url}");
+            // Создаём запрос для авторизации на сайте
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            // Создаём контейнер для Cookies
+            request.CookieContainer = new CookieContainer();
+            // Добавляем Cookie авторизированного пользователя
+            request.CookieContainer.Add(Token);
+            // Выполняем запрос, записывая результат в переменную response
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            // Выводим статус обращения
+            Debug.WriteLine($"Статус выполнения: {response.StatusCode}");
+            // Читаем ответ
+            string responseFromServer = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            Console.WriteLine(responseFromServer);
+        }
     }
 }
